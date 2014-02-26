27c2a0aff915d05125898f3f1e78550b8f39648a
cfc1020 <cfc9209@gmail.com>
2014-02-26 23:11:57 +0300

    Add controller Users and gem Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ProjectADS::Application.routes.draw do
+  get "users/show", as: "user_root"
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
