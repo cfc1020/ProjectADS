233ee0a005901245ee7dd41f1a9c04f48295d7b5
cfc1020 <cfc9209@gmail.com>
2014-02-28 20:49:36 +0300

    Add show all ads any user (simple)

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,7 @@
 class Ad < ActiveRecord::Base
 	belongs_to :user
-  	validates :user_id, presence: true
+  	#validates  :user_id, presence: true
+
+  	attr_accessible :content, :user_id
+  	#attr_protected :user_id
 end
