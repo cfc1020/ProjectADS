e956dbd0f726819123df3fe51f5f6b61ce8d8a56
cfc1020 <cfc9209@gmail.com>
2014-03-01 16:13:07 +0300

    Add state

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -5,4 +5,39 @@ class Ad < ActiveRecord::Base
 
   	attr_accessible :content, :user_id
   	#attr_protected :user_id
+
+
+  	state_machine initial: :draft do
+  		state :draft, value: 0
+    	state :pending, value: 1
+    	state :published, value: 2
+    	state :rejected, value: 3
+    	state :approved, value: 4
+    	state :archives, value: 5
+
+    	event :publish do
+      		transition :approved => :published
+    	end
+
+    	event :rejecte do
+      		transition :pending => :rejected
+    	end
+
+    	event :send_to_arch do
+      		transition :published => :archives
+    	end
+
+    	event :approve do
+      		transition :pending => :approved
+    	end
+
+    	event :send_to_pending do
+      		transition :draft => :pending
+    	end
+
+    	event :send_to_draft do
+      		transition [:published, :approved, :rejected, :pending, :archives] 
+      			=> :draft
+    	end
+  	end
 end
