3b522dda34dcd1c217ebf1c5eac696df90d5dea5
cfc1020 <cfc9209@gmail.com>
2014-03-07 19:11:38 +0300

    Reduced the number of line in the UsersController

--- app/controllers/ads_controller.rb
+++ app/controllers/ads_controller.rb
@@ -4,30 +4,17 @@ class AdsController < ApplicationController
 
   skip_before_filter :authenticate_user! , :only => [:index]
 
-  #before_action :get_ad, :only => [:edit, :destroy, :update, :transfer_state]
-
   def index
     @ads = Ad.paginate(:page => params[:page], :per_page => 5).
       find_all_by_state("published")
   end
 
   def new
-    if signed_in?
-      @ad = Ad.new
-      @ad.pictures.build #current_user.ads.build
-    else
-      redirect_to new_user_session_path
-    end
+    @ad.pictures.build #current_user.ads.build
   end
 
   def create
-    @ad = current_user.ads.build(params.require(:ad).permit(:content, :type_id))
-    if !params[:ad][:pictures_attributes].nil?
-      @ad.pictures_attributes = params[:ad][:pictures_attributes]
-    end
-
-
-
+    @ad.user = current_user
     if @ad.save
       flash[:notice] = 'OK!!!'
       redirect_to action: 'index'
@@ -37,12 +24,10 @@ class AdsController < ApplicationController
   end
 
   def edit
-    #@ad = Ad.find(params[:id])
   end
 
   def update
-    #@ad = Ad.find(params[:id])
-    if @ad.update_attributes(params[:ad])
+    if @ad.update_attributes(params[:ad]) 
       flash[:notice] = 'OK!.'
       redirect_to ads_path
     else
@@ -51,20 +36,11 @@ class AdsController < ApplicationController
   end
 
   def destroy
-    #@ad = Ad.find(params[:id])
     @ad.destroy
     redirect_to ads_path
   end
 
   def transfer_state
-    #@ad = Ad.find(params[:id])
     @ad.send(params[:transfer_method])
   end
-
-
-  #private
-# 
-  #def get_ad
- #   @ad = Ad.find(params[:id])
-  #end
 end
