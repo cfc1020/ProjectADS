dd2adc306a4f2b726c4c59b68dd960c89c6f284f
cfc1020 <cfc9209@gmail.com>
2014-03-25 15:12:25 +0300

    Add upload file from hard disk (Close #9)

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,10 +1,13 @@
 class Picture < ActiveRecord::Base
-  attr_accessible :url
+  attr_accessible :url, :photo
   attr_protected  :ad_id
 
   belongs_to      :ad
 
-  URL_PATTERN = /\A^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$\z/
+  #URL_PATTERN = /\A^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$\z/
 
-  validates :url, :presence => true, :format => { :with => URL_PATTERN }
+  #validates :url, :format => { :with => URL_PATTERN }
+
+  has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140322101003) do
+ActiveRecord::Schema.define(version: 20140325102709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,10 @@ ActiveRecord::Schema.define(version: 20140322101003) do
     t.integer  "ad_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "types", force: true do |t|
