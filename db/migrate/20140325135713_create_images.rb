11b029ff052efc165f982c39b8322ecc3976a32a
cfc1020 <cfc9209@gmail.com>
2014-03-25 21:26:33 +0300

    Fix bug in migrate Close #14

--- db/migrate/20140325135713_create_images.rb
+++ db/migrate/20140325135713_create_images.rb
@@ -1,7 +1,8 @@
 class CreateImages < ActiveRecord::Migration
   def change
     create_table :images do |t|
-
+      t.integer :ad_id
+      
       t.timestamps
     end
   end
--- db/migrate/20140325140942_add_ad_id_to_images.rb
+++
@@ -1,5 +0,0 @@
-class AddAdIdToImages < ActiveRecord::Migration
-  def change
-    add_column :images, :ad_id, :integer
-  end
-end
