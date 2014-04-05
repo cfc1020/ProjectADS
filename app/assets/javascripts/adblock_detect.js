f77275328f2e44288bd14ad089493d6b5760616c
cfc1020 <cfc9209@gmail.com>
2014-04-05 11:04:47 +0300

    Now there is no js in the views. Resolve #11

---
+++ app/assets/javascripts/adblock_detect.js
@@ -0,0 +1,8 @@
+$(document.body).append('<div id="advblock" style="position: absolute; opacity: 1; top: 0; left: 0;">hh</div>');
+setTimeout(function() {
+  if (!$('#advblock').height()) {
+    window.location.href = "/adblock_problem.html";
+  }
+  $("#advblock").remove();
+},
+1);
