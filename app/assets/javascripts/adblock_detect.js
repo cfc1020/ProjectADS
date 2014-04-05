91aa20bd2d5e0fae4aa15e1d5aa2dc19ec5deefe
cfc1020 <cfc9209@gmail.com>
2014-04-05 14:52:30 +0300

    Improved stability AdBlock detected

--- app/assets/javascripts/adblock_detect.js
+++ app/assets/javascripts/adblock_detect.js
@@ -1,12 +1,7 @@
-$(document.body).append('<div id="advblock" style="position: absolute; opacity: 1; top: 0; left: 0;">h</div>');
+$(document.body).append('<div id="advblock" style="position: absolute; opacity: 1; top: 0; left: 0;">&nbsp;</div>');
 setTimeout(function() {
   if (!$('#advblock').height()) {
     window.location.href = "/adblock_problem.html";
   }
 }, 
-1
-);
-setTimeout(function() {
-  $("#advblock").remove();
-}, 
-10);
+1);
