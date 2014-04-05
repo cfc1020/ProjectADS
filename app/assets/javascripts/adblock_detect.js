982e3b8b455cae15efe7bc150bac4d43a3135224
cfc1020 <cfc9209@gmail.com>
2014-04-05 11:36:03 +0300

    Fix bug in adblock_detected js

--- app/assets/javascripts/adblock_detect.js
+++ app/assets/javascripts/adblock_detect.js
@@ -1,8 +1,12 @@
-$(document.body).append('<div id="advblock" style="position: absolute; opacity: 1; top: 0; left: 0;">hh</div>');
+$(document.body).append('<div id="advblock" style="position: absolute; opacity: 1; top: 0; left: 0;">h</div>');
 setTimeout(function() {
   if (!$('#advblock').height()) {
     window.location.href = "/adblock_problem.html";
   }
+}, 
+1
+);
+setTimeout(function() {
   $("#advblock").remove();
-},
-1);
+}, 
+10);
