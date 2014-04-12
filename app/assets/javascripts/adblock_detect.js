5cf95bedc056f2e5da21b2614e2ab43a1ee37b33
cfc1020 <cfc9209@gmail.com>
2014-04-12 18:05:49 +0300

    Fix

--- app/assets/javascripts/adblock_detect.js
+++
@@ -1,7 +0,0 @@
-$(document.body).append('<div id="advblock" style="position: absolute; opacity: 1; top: 0; left: 0;">&nbsp;</div>');
-setTimeout(function() {
-  if (!$('#advblock').height()) {
-    window.location.href = "/adblock_problem.html";
-  }
-}, 
-100);
